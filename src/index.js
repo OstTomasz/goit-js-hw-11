be95f69a34b8ca79c414ac0e8dba702a7331de6e
Tomasz Ostaszewski <114577269+OstTomasz@users.noreply.github.com>
2023-04-08 15:36:49 +0200

    scss done

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import './sass/index.scss';
 
 const searchForm = document.querySelector('.search-form');
 let searchFormInput = document.querySelector(`input[name="searchQuery"]`);
 let searchString;
-const picturesList = document.querySelector('.pictures-list');
+const picturesList = document.querySelector('.pictures');
 
 searchForm.addEventListener('submit', e => {
   e.preventDefault();
@@ -11,22 +12,36 @@ searchForm.addEventListener('submit', e => {
   const APIURL = `https://pixabay.com/api/`;
   const params = new URLSearchParams({
     key: '35169635-92091552d9eccdba3eb57d7a9',
-    q: searchString,
+    q: 'dog in the fog',
+    // q: searchString,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: 'true',
   });
+
   const fetchPictures = async () => {
     const response = await fetch(APIURL + '?' + params);
-
     if (!response.ok) {
       throw new Error(response.statusText);
     }
     const pictures = await response.json();
-
-    console.log('pictures:', pictures);
-    return pictures;
+    return pictures.hits;
   };
 
-  fetchPictures();
+  fetchPictures().then(pictures => {
+    console.log(pictures);
+    picturesList.innerHTML = pictures
+      .map(picture => {
+        return `<li class="pictures-element">
+            <img class="pictures-element_pic" src=${picture.webformatURL} alt=${picture.tags}>
+          <div class="pictures-element_pic-description">
+          <div class = "pictures-element_pic-description-element"><p class="pictures-element_pic-description-element_header">Likes</p><p class="pictures-element_value">${picture.likes}</p></div>
+          <div class = "pictures-element_pic-description-element"><p class="pictures-element_pic-description-element_header">Views</p><p class="pictures-element_value">${picture.views}</p></div>
+          <div class = "pictures-element_pic-description-element"><p class="pictures-element_pic-description-element_header">Comments</p><p class="pictures-element_value">${picture.comments}</p></div>
+          <div class = "pictures-element_pic-description-element"><p class="pictures-element_pic-description-element_header">Downloads</p><p class="pictures-element_value">${picture.downloads}</p></div>
+          </div>
+          </li>`;
+      })
+      .join('');
+  });
 });
