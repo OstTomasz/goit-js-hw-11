467b552284efbe9342de061e9f12f88d37507ccb
Tomasz Ostaszewski <ost.tomasz@gmail.com>
2023-04-11 18:18:26 +0200

    pagination

--- src/index.js
+++ src/index.js
@@ -5,39 +5,50 @@ const searchForm = document.querySelector('.search-form');
 let searchFormInput = document.querySelector(`input[name="searchQuery"]`);
 let searchString;
 const picturesList = document.querySelector('.pictures');
+const loadMore = document.querySelector('.load-more');
+let listMarkup = '';
+let page = 1;
 
 searchForm.addEventListener('submit', e => {
+  page = 1;
   e.preventDefault();
   searchString = searchFormInput.value;
   const APIURL = `https://pixabay.com/api/`;
   const params = new URLSearchParams({
     key: '35169635-92091552d9eccdba3eb57d7a9',
-    // q: 'dog in the fog',
     q: searchString,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: 'true',
+    per_page: 40,
   });
 
-  const fetchPictures = async () => {
-    const response = await fetch(APIURL + '?' + params);
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    }
-    const pictures = await response.json();
-    return pictures.hits;
-  };
+  if (searchString) {
+    const fetchPictures = async page => {
+      const response = await fetch(APIURL + '?' + params + '&page=' + page);
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      const pictures = await response.json();
+      if (page === 1) {
+        if (pictures.hits.length > 0) {
+          Notify.info(`Hooray! We found ${pictures.total} images.`);
+        }
+      }
+      return pictures.hits;
+    };
 
-  fetchPictures().then(pictures => {
-    console.log(pictures);
-    if (pictures.length === 0) {
-      Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-    }
-    picturesList.innerHTML = pictures
-      .map(picture => {
-        return `<li class="pictures-element">
+    fetchPictures(page)
+      .then(pictures => {
+        if (pictures.length === 0) {
+          loadMore.style.visibility = 'hidden';
+          Notify.failure(
+            'Sorry, there are no images matching your search query. Please try again.'
+          );
+        }
+        listMarkup = pictures
+          .map(picture => {
+            return `<li class="pictures-element">
             <img class="pictures-element_pic" src=${picture.webformatURL} alt=${picture.tags} loading="lazy" />
           <div class="pictures-element_pic-description">
           <div class = "pictures-element_pic-description-element"><p class="pictures-element_pic-description-element_header">Likes</p><p class="pictures-element_value">${picture.likes}</p></div>
@@ -46,17 +57,55 @@ searchForm.addEventListener('submit', e => {
           <div class = "pictures-element_pic-description-element"><p class="pictures-element_pic-description-element_header">Downloads</p><p class="pictures-element_value">${picture.downloads}</p></div>
           </div>
           </li>`;
+          })
+          .join('');
+        picturesList.innerHTML = listMarkup;
       })
-      .join('');
-  });
+      .finally(() => {
+        if (picturesList.innerHTML !== '') {
+          loadMore.style.visibility = 'visible';
+        }
+      });
+
+    loadMore.addEventListener('click', () => {
+      page = page + 1;
+      loadMore.style.visibility = 'hidden';
+      fetchPictures(page)
+        .then(pictures => {
+          listMarkup = pictures
+            .map(picture => {
+              return `<li class="pictures-element">
+            <img class="pictures-element_pic" src=${picture.webformatURL} alt=${picture.tags} loading="lazy" />
+          <div class="pictures-element_pic-description">
+          <div class = "pictures-element_pic-description-element"><p class="pictures-element_pic-description-element_header">Likes</p><p class="pictures-element_value">${picture.likes}</p></div>
+          <div class = "pictures-element_pic-description-element"><p class="pictures-element_pic-description-element_header">Views</p><p class="pictures-element_value">${picture.views}</p></div>
+          <div class = "pictures-element_pic-description-element"><p class="pictures-element_pic-description-element_header">Comments</p><p class="pictures-element_value">${picture.comments}</p></div>
+          <div class = "pictures-element_pic-description-element"><p class="pictures-element_pic-description-element_header">Downloads</p><p class="pictures-element_value">${picture.downloads}</p></div>
+          </div>
+          </li>`;
+            })
+            .join('');
+          picturesList.insertAdjacentHTML('beforeend', listMarkup);
+        })
+        .then(() => {
+          loadMore.style.visibility = 'visible';
+        })
+        .catch(() => {
+          loadMore.style.visibility = 'hidden';
+          Notify.failure(
+            "We're sorry, but you've reached the end of search results."
+          );
+        });
+    });
+  }
 });
 
-const { height: cardHeight } =
-  picturesList.firstElementChild.getBoundingClientRect();
+// const { height: cardHeight } =
+//   picturesList.firstElementChild.getBoundingClientRect();
 
-window.scrollBy({
-  top: cardHeight * 2,
-  behavior: 'smooth',
-});
+// window.scrollBy({
+//   top: cardHeight * 2,
+//   behavior: 'smooth',
+// });
 
 // ADD PAGINATION picturesList.addEventListener('scroll', e => {});
