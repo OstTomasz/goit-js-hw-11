57286fd06868bfe82c627539baac77bcb320c7c2
Tomasz Ostaszewski <ost.tomasz@gmail.com>
2024-09-21 16:21:33 +0200

    rendering Gallery

--- src/01-gallery.js
+++ src/01-gallery.js
@@ -11,7 +11,93 @@ const gallery = document.querySelector('#gallery');
 let searchString = '';
 const APIURL = `https://pixabay.com/api/`;
 
+const element = (tag, props) =>
+  Object.assign(document.createElement(tag), props);
+
+const createGalEl = ({
+  webformatURL,
+  largeImageURL,
+  tags,
+  likes,
+  views,
+  comments,
+  downloads,
+}) => {
+  const galEl = element('li', { className: 'result' });
+
+  const img = element('img', {
+    className: 'result-pic',
+    src: webformatURL,
+    alt: tags,
+  });
+  img.dataset.source = largeImageURL;
+
+  const description = element('div', { className: 'result-desc' });
+
+  const likesEl = element('div', { className: 'desc-el' });
+  const likesHead = element('p', {
+    className: 'desc-header',
+    textContent: 'Likes',
+  });
+  const likesVal = element('p', {
+    className: 'desc-value',
+    textContent: likes,
+  });
+
+  likesEl.append(likesHead, likesVal);
+
+  const viewsEl = element('div', { className: 'desc-el' });
+  const viewsHead = element('p', {
+    className: 'desc-header',
+    textContent: 'Views',
+  });
+  const viewsVal = element('p', {
+    className: 'desc-value',
+    textContent: views,
+  });
+
+  viewsEl.append(viewsHead, viewsVal);
+
+  const comEl = element('div', { className: 'desc-el' });
+  const comHead = element('p', {
+    className: 'desc-header',
+    textContent: 'Comments',
+  });
+  const comVal = element('p', {
+    className: 'desc-value',
+    textContent: comments.toString(),
+  });
+
+  comEl.append(comHead, comVal);
+
+  const downloadEl = element('div', { className: 'desc-el' });
+  const downloadHead = element('p', {
+    className: 'desc-header',
+    textContent: 'Downloads',
+  });
+  const downloadVal = element('p', {
+    className: 'desc-value',
+    textContent: downloads,
+  });
+
+  downloadEl.append(downloadHead, downloadVal);
+
+  description.append(likesEl, viewsEl, comEl, downloadEl);
+
+  galEl.append(img, description);
+
+  return galEl;
+};
+
+const renderElements = (images, rootList) => {
+  const fragment = document.createDocumentFragment();
+  fragment.append(...images.map(createGalEl));
+
+  rootList.append(fragment);
+};
+
 form.addEventListener('submit', e => {
+  gallery.innerHTML = '';
   e.preventDefault();
   searchString = input.value;
   const params = new URLSearchParams({
@@ -19,7 +105,7 @@ form.addEventListener('submit', e => {
     q: searchString,
     image_type: 'photo',
     orientation: 'horizontal',
-    safesearch: 'false',
+    safesearch: 'true',
   });
 
   if (searchString !== '') {
@@ -39,8 +125,13 @@ form.addEventListener('submit', e => {
               'Sorry, there are no images matching your search query. Please try again!',
           });
         }
-        console.log(response.hits);
+        const hits = response.hits;
         input.value = '';
+        return hits;
+      })
+      .then(hits => {
+        renderElements(hits, gallery);
+        console.log(hits);
       })
       .catch(error => console.log(error));
   } else {
